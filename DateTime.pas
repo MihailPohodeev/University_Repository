61554d92384a801beb40b405be1a48fdf7234f42
Pohodeev Mihail <pokhodeev@sfedu.ru>
2022-10-06 14:31:47 +0300

    Добавил вычисление високосности года

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,12 @@
 ﻿begin
   
-  var c := ReadInteger('Input:');
-  
-  Writeln('Output: ',c);
+  var c := ReadInteger('Input year:');
+  if(((c mod 4 = 0) and (c mod 100 <> 0))or (c mod 400 = 0)) then
+  begin
+    
+    Println('+');
+    
+  end
+  else  Writeln('-');
   
 end.
